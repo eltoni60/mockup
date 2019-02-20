7f0404c887b6bd3cf8e9a8de62d9d591d5e0d755
Jose Rodriguez <jrodr3502@365.elmhurst.edu>
2019-02-20 03:57:56 -0600

    the place where i left off

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FoodButtonsComponent} from './food-buttons/food-buttons.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {NavHeaderComponent} from './nav-header/nav-header.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
+import {GridLayoutComponent} from './grid-layout/grid-layout.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {ReceiptListComponent} from './receipt-list/receipt-list.component';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodButtonsComponent,
+    NavHeaderComponent,
+    GridLayoutComponent,
+    ReceiptListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/mock-entrees.ts
@@ -0,0 +1,11 @@
+import {Entree} from './menu-item';
+
+
+export const ENTREES: Entree[] = [
+  {entreeId: 1, name: 'Burger w/Fries', cost: 5.99, inventory: [['bun', 999], ['beef', 998], ['cheese', 997], ['fries', 996]]},
+  {entreeId: 2, name: 'Fries', cost: 2.99, inventory: [['potato fries', 995]]},
+  {entreeId: 3, name: 'Fountain Drink', cost: 0.98, inventory: [['liquid flavoring', 994], ['carbonation', 993]]}
+];
+
+
+
